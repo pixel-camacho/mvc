07c347619519d560cd57b3e5c1e7492034072314
pixel-camacho <62088308+pixel-camacho@users.noreply.github.com>
2024-02-11 22:54:00 -0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
+define('DB_HOST','');
+define('DB_USER','');
 define('DB_PASS','');
-define('DB_NAME','mvc');
+define('DB_NAME','');
